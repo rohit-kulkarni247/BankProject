fbcc82910ac8dbfa601c60c2fa03e232ed4dc68e
Rohit <rohitkul247@gmail.com>
2020-08-29 16:44:56 +0530

    removed port

--- app.js
+++ app.js
@@ -237,12 +237,7 @@ app.post('/withdraw',function(req,res){
 });
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 
-
-app.listen(port,function(){
+app.listen(process.env.PORT || 3000 ,function(){
   console.log("server running on port 3000");
 });
