101bb3ea829eff5fb97cbec5bb056cf8fe618c0e
Rohit <rohitkul247@gmail.com>
2020-10-30 15:01:18 +0530

    added to something file

--- something.sh
+++ something.sh
@@ -2,3 +2,5 @@
 
 export CLIENT_ID=1026627563714-2v3q5uo0mgktl758u2bvboa1ni75ppf6.apps.googleusercontent.com;
 export CLIENT_SECRET=v628e7s5-z3EEZYoCi3cCxwV;
+export CLIENT_ID_GITHUB=19496df3fe27f437e4c9
+export CLIENT_SECRET_GITHUB=70e58a2da348ba3566ae74d944fe359bfef540d0
